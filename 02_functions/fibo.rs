533be0148089c06218435bbc4ad16c9e59839dfb
Buff2out <slava.070299@gmail.com>
2024-11-01 15:57:19 +0300

    refactored

--- 02_functions/fibo.rs
+++ 02_functions/fibo.rs
@@ -1,17 +1,15 @@
 use std::io;
 
 fn fibo(n: i32) -> i32 {
-    if n > 2 {
+    if n > 1 {
         return fibo(n - 1) + fibo(n - 2);
     } else {
-        return 1;
+        return n;
     }
 }
 
 fn check_valid(n: i32) -> i32 {
     if n < 0 {
-        -2
-    } else if 0 == n {
         -1
     } else {
         n
@@ -23,10 +21,8 @@ fn main() {
     io::stdin().read_line(&mut n).expect("failed to readline");
     let n: i32 = n.trim().parse().expect("inputed is not a num");
     let valid = check_valid(n);
-    if -2 == valid {
+    if -1 == valid {
         println!("Inputed number less than zero");
-    } else if -1 == valid {
-        println!("Result 0")
     } else {
         let res = fibo(n);
         println!("Result {res}");
