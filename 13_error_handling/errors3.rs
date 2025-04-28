bca18de276cb7eae25f7f481c42e1e2abbae2464
rorikstr <slava.070299@gmail.com>
2025-04-28 22:53:09 +0300

    done

--- 13_error_handling/errors3.rs
+++ 13_error_handling/errors3.rs
@@ -15,7 +15,7 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
 
 // TODO: Fix the compiler error by changing the signature and body of the
 // `main` function.
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
@@ -28,4 +28,45 @@ fn main() {
         tokens -= cost;
         println!("You now have {tokens} tokens.");
     }
+    Ok(())
 }
+
+/*
+// Alternate solution
+
+use std::num::ParseIntError;
+
+fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+    let processing_fee = 1;
+    let cost_per_item = 5;
+    
+    // Явная обработка Result с match вместо оператора ?
+    let parse_result = item_quantity.parse::<i32>();
+    let qty = match parse_result {
+        Ok(n) => n,
+        Err(e) => return Err(e),
+    };
+    
+    Ok(qty * cost_per_item + processing_fee)
+}
+
+fn main() {
+    let mut tokens = 100;
+    let pretend_user_input = "8";
+    
+    // Обработка результата через match
+    match total_cost(pretend_user_input) {
+        Ok(cost) => {
+            if cost > tokens {
+                println!("You can't afford that many!");
+            } else {
+                tokens -= cost;
+                println!("You now have {tokens} tokens.");
+            }
+        },
+        Err(e) => {
+            eprintln!("Error calculating cost: {}", e);
+        }
+    }
+}
+*/
